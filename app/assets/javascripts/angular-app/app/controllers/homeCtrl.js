f4555b24f9c1e7791161d1d8e442ce5918e88cb7
jayant-parchure@uiowa.edu <jayant-parchure@uiowa.edu>
2015-02-22 15:17:51 -0600

    The commit before master

--- app/assets/javascripts/angular-app/app/controllers/homeCtrl.js
+++ app/assets/javascripts/angular-app/app/controllers/homeCtrl.js
@@ -1,6 +1,11 @@
 /**
  * Created by saasbook on 09/02/15.
  */
-app.controller("homeCtrl", function(){
-
-});
+app.controller("homeCtrl", ['$scope', '$http',function($scope, $http){
+	function getData(){
+		http.get('/').success(function(data){
+			$scope.currentuser=data;
+		})
+	}
+}]);
+//$location for angular routes
