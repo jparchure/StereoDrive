5eb2a2f7d50ca89e3db12be2eda351f6dcc68c2c
jayant-parchure@uiowa.edu <jayant-parchure@uiowa.edu>
2015-02-22 16:36:24 -0600

    link to fb api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,7 @@ class ApplicationController < ActionController::Base
   end
   def send_current_user
     @current_user ||= session[:token] && User.find_by_session(session[:token])
-    respond_to do |format|
-	format.json { render json: @current_user }
-    end
+	render json: @current_user
   end
 
 end
