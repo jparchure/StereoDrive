5c52e2ef4cba51c65cee753bbc9ffa8e7932a5a0
kalbozo <kaleb-bozorgzadeh@uiowa.edu>
2015-03-18 17:30:10 -0500

    added create methods for artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,8 @@
 class Artist < ActiveRecord::Base
 has_and_belongs_to_many :users
+  attr_accessible :name, :image, :page, :location, :genre
+  def self.create_artist! (hash)
+	Artist.create!(hash)
+  end
+
 end
