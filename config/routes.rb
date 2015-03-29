36d06040a2434ee2432c3ad129741912468d30b7
kyleclark171 <kyleclark71@gmail.com>
2015-03-29 14:37:28 -0500

    got rid of unneccesary routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Team7::Application.routes.draw do
   post '/track' => 'track#create'
   get '/track' => 'track#index'
   post '/deleteTrack' => 'track#delete'
-  get  '/paytonleevieno' => 'application#send_current_user'
-
   post '/clips' => 'clips#create'
   post '/clips/:id' => 'clips#update'
   get 'audio/:key/delete' => 'audio#delete'
