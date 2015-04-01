1ebdf7fbbd40c8679d405dbcd26aed81ee5ed664
kalbozo <kaleb-bozorgzadeh@uiowa.edu>
2015-04-01 12:11:02 -0500

    finished session controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,18 +3,28 @@ require 'rails_helper'
 describe SessionsController do
   describe 'Logging in' do
 	before { visit root_path}
-	it "can sign in user with Twitter account" do
+	it "can sign in user with Facebook account" do
 	    page.should have_content("StereoDrive")
 	    facebook_hash
 	    click_link "Login"
 	    page.should have_content("Logout")
 	  end
+	it "will ensure user has a SOLO BAND" do
+	    facebook_hash
+	    click_link "Login"
+	    user = User.first
+	    expect(user.artists.count).to eq(1)
+	  end
   end
 
   describe 'Logging out' do
+    before do 
+	visit root_path
+	facebook_hash
+	click_link "Login"
+    end
     it 'should remove session variable' do
-      session[:token] = "test_user"
-      delete :destroy
+      click_link "Logout"
       expect(session[:token]).to equal(nil)
     end
   end
